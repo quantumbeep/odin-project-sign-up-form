cb7f98335330691dcadac30fe726abb78ee94d6d
quantumbeep <kh@khs-mac-mini.lan>
2022-06-16 16:18:22 -0700

    add client side field validations add javascript password matching check

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const checkSame = () => {
-    const password = document.querySelector("error").value
-    password.value
-    if ()
-}
+  const password = document.getElementById('password').value;
+  const passwordCopy = document.getElementById('confirm').value;
+  if (password === passwordCopy) {
+    document.getElementById('message').innerHTML = 'Passwords match.';
+    document.getElementById('password').style.border = '3px solid green';
+    document.getElementById('confirm').style.border = '3px solid green';
+  } else {
+    document.getElementById('message').innerHTML = 'Passwords do not match.';
+    document.getElementById('password').style.border = '3px solid red';
+    document.getElementById('confirm').style.border = '3px solid red';
+  }
+};
